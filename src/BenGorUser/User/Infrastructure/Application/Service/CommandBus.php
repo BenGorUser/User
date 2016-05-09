1d04fbf8f0ba84e4836be69061574d2bcffc2e73
benatespina <benatespina@gmail.com>
2016-05-09 18:14:56 +0200

    Added command bus interface

---
+++ src/BenGorUser/User/Infrastructure/Application/Service/CommandBus.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the BenGorUser package.
+ *
+ * (c) Beñat Espiña <benatespina@gmail.com>
+ * (c) Gorka Laucirica <gorka.lauzirika@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace BenGorUser\User\Infrastructure\Application\Service;
+
+/**
+ * Command bus class.
+ *
+ * @author Beñat Espiña <benatespina@gmail.com>
+ * @author Gorka Laucirica <gorka.lauzirika@gmail.com>
+ */
+interface CommandBus
+{
+    /**
+     * Executes the given command and optionally returns a value.
+     *
+     * @param mixed $aCommand The command given
+     *
+     * @return mixed
+     */
+    public function handle($aCommand);
+}
