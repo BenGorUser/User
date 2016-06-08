a1541d71a5e1bc5cc3f1794cf530848fe91bce30
gorkalaucirica <gorka.lauzirika@gmail.com>
2016-06-08 17:28:40 +0200

    Added UserCommandBus to infraestructure to allow command bus abstraction

---
+++ src/BenGorUser/User/Infrastructure/CommandBus/UserCommandBus.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the BenGorUser package.
+ *
+ * (c) Beñat Espiña <benatespina@gmail.com>
+ * (c) Gorka Laucirica <gorka.lauzirika@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace BenGorUser\User\CommandBus;
+
+/**
+ * User command bus class.
+ *
+ * @author Beñat Espiña <benatespina@gmail.com>
+ * @author Gorka Laucirica <gorka.lauzirika@gmail.com>
+ */
+interface UserCommandBus
+{
+    /**
+     * Executes the given command.
+     *
+     * @param mixed $aCommand The command given
+     */
+    public function handle($aCommand);
+}
