ff4684094ad2ca28e18afbf157a8b60bab65a936
gorkalaucirica <gorka.lauzirika@gmail.com>
2016-06-08 17:33:02 +0200

    Fixed UserCommandBus namespace

--- src/BenGorUser/User/Infrastructure/CommandBus/UserCommandBus.php
+++ src/BenGorUser/User/Infrastructure/CommandBus/UserCommandBus.php
@@ -10,7 +10,7 @@
  * file that was distributed with this source code.
  */
 
-namespace BenGorUser\User\CommandBus;
+namespace BenGorUser\User\Infrastructure\CommandBus;
 
 /**
  * User command bus class.
